fd00d384d9fb8950ed53da891919b215f3447649
qianxu2001 <62287903+qianxu2001@users.noreply.github.com>
2020-08-18 21:57:03 +0800

    Update mainmenu.c

--- source/fbi/mainmenu.c
+++ source/fbi/mainmenu.c
@@ -7,19 +7,19 @@
 #include "section.h"
 #include "../core/core.h"
 
-static list_item sd = {"SD", COLOR_TEXT, files_open_sd};
+static list_item sd = {"SD 卡", COLOR_TEXT, files_open_sd};
 static list_item ctr_nand = {"CTR NAND", COLOR_TEXT, files_open_ctr_nand};
 static list_item twl_nand = {"TWL NAND", COLOR_TEXT, files_open_twl_nand};
-static list_item twl_photo = {"TWL Photo", COLOR_TEXT, files_open_twl_photo};
-static list_item twl_sound = {"TWL Sound", COLOR_TEXT, files_open_twl_sound};
-static list_item dump_nand = {"Dump NAND", COLOR_TEXT, dumpnand_open};
+static list_item twl_photo = {"TWL 图片", COLOR_TEXT, files_open_twl_photo};
+static list_item twl_sound = {"TWL 音频", COLOR_TEXT, files_open_twl_sound};
+static list_item dump_nand = {"导出 NAND", COLOR_TEXT, dumpnand_open};
 static list_item titles = {"Titles", COLOR_TEXT, titles_open};
-static list_item pending_titles = {"Pending Titles", COLOR_TEXT, pendingtitles_open};
+static list_item pending_titles = {"未完成的 Titles", COLOR_TEXT, pendingtitles_open};
 static list_item tickets = {"Tickets", COLOR_TEXT, tickets_open};
-static list_item ext_save_data = {"Ext Save Data", COLOR_TEXT, extsavedata_open};
-static list_item system_save_data = {"System Save Data", COLOR_TEXT, systemsavedata_open};
-static list_item remote_install = {"Remote Install", COLOR_TEXT, remoteinstall_open};
-static list_item update = {"Update", COLOR_TEXT, update_open};
+static list_item ext_save_data = {"额外数据", COLOR_TEXT, extsavedata_open};
+static list_item system_save_data = {"系统数据", COLOR_TEXT, systemsavedata_open};
+static list_item remote_install = {"远程安装", COLOR_TEXT, remoteinstall_open};
+static list_item update = {"检查更新", COLOR_TEXT, update_open};
 
 static void mainmenu_draw_top(ui_view* view, void* data, float x1, float y1, float x2, float y2, list_item* selected) {
     u32 logoWidth;
@@ -64,5 +64,5 @@ static void mainmenu_update(ui_view* view, void* data, linked_list* items, list_
 void mainmenu_open() {
     resources_load();
 
-    list_display("Main Menu", "A: Select, START: Exit", NULL, mainmenu_update, mainmenu_draw_top);
+    list_display("主菜单", "A：选择, START：退出", NULL, mainmenu_update, mainmenu_draw_top);
 }
