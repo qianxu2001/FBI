f9e1ffa771ad22f0363cbcd45f2a1540f5cc5172
qianxu2001 <62287903+qianxu2001@users.noreply.github.com>
2020-08-18 18:51:10 +0800

    Update dumpnand.c

--- source/fbi/dumpnand.c
+++ source/fbi/dumpnand.c
@@ -80,7 +80,7 @@ static Result dumpnand_restore(void* data, u32 index) {
 }
 
 static bool dumpnand_error(void* data, u32 index, Result res, ui_view** errorView) {
-    *errorView = error_display_res(NULL, NULL, res, "Failed to dump NAND.");
+    *errorView = error_display_res(NULL, NULL, res, "无法导出 NAND。");
     return true;
 }
 
@@ -92,7 +92,7 @@ static void dumpnand_update(ui_view* view, void* data, float* progress, char* te
         info_destroy(view);
 
         if(R_SUCCEEDED(dumpData->result)) {
-            prompt_display_notify("Success", "NAND dumped.", COLOR_TEXT, NULL, NULL, NULL);
+            prompt_display_notify("成功", "已导出 NAND。", COLOR_TEXT, NULL, NULL, NULL);
         }
 
         free(dumpData);
@@ -118,9 +118,9 @@ static void dumpnand_onresponse(ui_view* view, void* data, u32 response) {
 
         Result res = task_data_op(dumpData);
         if(R_SUCCEEDED(res)) {
-            info_display("Dumping NAND", "Press B to cancel.", true, data, dumpnand_update, NULL);
+            info_display("正在导出 NAND", "按 B 取消。", true, data, dumpnand_update, NULL);
         } else {
-            error_display_res(NULL, NULL, res, "Failed to initiate NAND dump.");
+            error_display_res(NULL, NULL, res, "无法初始化导出 NAND。");
             free(data);
         }
     } else {
@@ -131,7 +131,7 @@ static void dumpnand_onresponse(ui_view* view, void* data, u32 response) {
 void dumpnand_open() {
     data_op_data* data = (data_op_data*) calloc(1, sizeof(data_op_data));
     if(data == NULL) {
-        error_display(NULL, NULL, "Failed to allocate dump NAND data.");
+        error_display(NULL, NULL, "无法分配导出 NAND 数据。");
 
         return;
     }
@@ -164,5 +164,5 @@ void dumpnand_open() {
 
     data->finished = true;
 
-    prompt_display_yes_no("Confirmation", "Dump raw NAND image to the SD card?", COLOR_TEXT, data, NULL, dumpnand_onresponse);
+    prompt_display_yes_no("确认", "导出原始 NAND 镜像至 SD 卡？", COLOR_TEXT, data, NULL, dumpnand_onresponse);
 }
