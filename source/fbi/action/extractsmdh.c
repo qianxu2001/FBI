7dfd71a0ef200547e2f59a2c31f4b6225662b97d
qianxu2001 <62287903+qianxu2001@users.noreply.github.com>
2020-08-19 16:25:17 +0800

    Update extractsmdh.c

--- source/fbi/action/extractsmdh.c
+++ source/fbi/action/extractsmdh.c
@@ -61,18 +61,18 @@ static void action_extract_smdh_update(ui_view* view, void* data, float* progres
     info_destroy(view);
 
     if(R_SUCCEEDED(res)) {
-        prompt_display_notify("Success", "SMDH extracted.", COLOR_TEXT, info, task_draw_title_info, NULL);
+        prompt_display_notify("成功", "已提取 SMDH.", COLOR_TEXT, info, task_draw_title_info, NULL);
     } else {
-        error_display_res(info, task_draw_title_info, res, "Failed to extract SMDH.");
+        error_display_res(info, task_draw_title_info, res, "无法提取 SMDH.");
     }
 }
 
 static void action_extract_smdh_onresponse(ui_view* view, void* data, u32 response) {
     if(response == PROMPT_YES) {
-        info_display("Extracting SMDH", "", false, data, action_extract_smdh_update, task_draw_title_info);
+        info_display("正在提取 SMDH", "", false, data, action_extract_smdh_update, task_draw_title_info);
     }
 }
 
 void action_extract_smdh(linked_list* items, list_item* selected) {
-    prompt_display_yes_no("Confirmation", "Extract the SMDH of the selected title?", COLOR_TEXT, selected->data, task_draw_title_info, action_extract_smdh_onresponse);
+    prompt_display_yes_no("确认", "提取所选 Title 的 SMDH?", COLOR_TEXT, selected->data, task_draw_title_info, action_extract_smdh_onresponse);
 }
