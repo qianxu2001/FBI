3e55f8542ebfb3a2fde0d0a4ffbb5a1c4d9fb2a1
qianxu2001 <62287903+qianxu2001@users.noreply.github.com>
2020-08-19 16:06:14 +0800

    Update deletesecurevalue.c

--- source/fbi/action/deletesecurevalue.c
+++ source/fbi/action/deletesecurevalue.c
@@ -18,18 +18,18 @@ static void action_delete_secure_value_update(ui_view* view, void* data, float* 
     info_destroy(view);
 
     if(R_FAILED(res)) {
-        error_display_res(info, task_draw_title_info, res, "Failed to delete secure value.");
+        error_display_res(info, task_draw_title_info, res, "无法删除安全值.");
     } else {
-        prompt_display_notify("Success", "Secure value deleted.", COLOR_TEXT, info, task_draw_title_info, NULL);
+        prompt_display_notify("成功", "已删除安全值.", COLOR_TEXT, info, task_draw_title_info, NULL);
     }
 }
 
 static void action_delete_secure_value_onresponse(ui_view* view, void* data, u32 response) {
     if(response == PROMPT_YES) {
-        info_display("Deleting Secure Value", "", false, data, action_delete_secure_value_update, task_draw_title_info);
+        info_display("正在删除安全值", "", false, data, action_delete_secure_value_update, task_draw_title_info);
     }
 }
 
 void action_delete_secure_value(linked_list* items, list_item* selected) {
-    prompt_display_yes_no("Confirmation", "Delete the secure value of the selected title?", COLOR_TEXT, selected->data, task_draw_title_info, action_delete_secure_value_onresponse);
+    prompt_display_yes_no("确认", "删除所选 Title 的安全值?", COLOR_TEXT, selected->data, task_draw_title_info, action_delete_secure_value_onresponse);
 }
