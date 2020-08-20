b6614199a3c090b3b90b546276c879ab4aae808b
qianxu2001 <62287903+qianxu2001@users.noreply.github.com>
2020-08-20 17:32:46 +0800

    Update erasetwlsave.c

--- source/fbi/action/erasetwlsave.c
+++ source/fbi/action/erasetwlsave.c
@@ -75,7 +75,7 @@ static Result action_erase_twl_save_restore(void* data, u32 index) {
 }
 
 static bool action_erase_twl_save_error(void* data, u32 index, Result res, ui_view** errorView) {
-    *errorView = error_display_res(((erase_twl_save_data*) data)->title, task_draw_title_info, res, "无法清除存档.");
+    *errorView = error_display_res(((erase_twl_save_data*) data)->title, task_draw_title_info, res, "无法清除数据.");
     return true;
 }
 
@@ -118,7 +118,7 @@ static void action_erase_twl_save_onresponse(ui_view* view, void* data, u32 resp
         if(R_SUCCEEDED(res)) {
             info_display("正在清除", "按 B 取消.", true, data, action_erase_twl_save_update, action_erase_twl_save_draw_top);
         } else {
-            error_display_res(eraseData->title, task_draw_title_info, res, "无法启动清除存档.");
+            error_display_res(eraseData->title, task_draw_title_info, res, "无法启动数据清除.");
             free(data);
         }
     } else {
@@ -129,7 +129,7 @@ static void action_erase_twl_save_onresponse(ui_view* view, void* data, u32 resp
 void action_erase_twl_save(linked_list* items, list_item* selected) {
     erase_twl_save_data* data = (erase_twl_save_data*) calloc(1, sizeof(erase_twl_save_data));
     if(data == NULL) {
-        error_display(NULL, NULL, "无法分配清除 TWL 存档的数据.");
+        error_display(NULL, NULL, "无法分配清除 TWL 数据的数据.");
 
         return;
     }
@@ -164,5 +164,5 @@ void action_erase_twl_save(linked_list* items, list_item* selected) {
 
     data->eraseInfo.finished = true;
 
-    prompt_display_yes_no("确认", "清除所选应用的存档?", COLOR_TEXT, data, action_erase_twl_save_draw_top, action_erase_twl_save_onresponse);
+    prompt_display_yes_no("确认", "清除所选应用的数据?", COLOR_TEXT, data, action_erase_twl_save_draw_top, action_erase_twl_save_onresponse);
 }
